f96c745f1ee0218d8b8a4c24187c89fe439c6d67
Semyon Levin <levin.semen@gmail.com>
2021-04-19 17:33:43 +0300

    test

--- src/main/java/name/remal/gradleplugins/template/WrongClass.java
+++ src/main/java/name/remal/gradleplugins/template/WrongClass.java
@@ -3,4 +3,7 @@ package name.remal.gradleplugins.template;
 public class WrongClass {
     public static <Wrong_Var> void main(String[] args) {
     }
+
+    public static <Wrong_Var> void main1(String[] args) {
+    }
 }
