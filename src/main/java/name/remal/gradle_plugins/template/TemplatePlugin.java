afe965b1abe2fb6566e54986d0ee9f9e135f4d8d
Semyon Levin <231188+remal@users.noreply.github.com>
2024-11-12 17:49:39 -0800

    [push-back] Update TemplatePlugin.java

--- src/main/java/name/remal/gradle_plugins/template/TemplatePlugin.java
+++ src/main/java/name/remal/gradle_plugins/template/TemplatePlugin.java
@@ -3,7 +3,7 @@ package name.remal.gradle_plugins.template;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-public class TemplatePlugin implements Plugin<Project> {
+public abstract class TemplatePlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
